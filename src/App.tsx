dd9e57b70a8f75f4a27703fc848a552582c995ff
Rick Machado <contato.rickmachado@gmail.com>
2022-04-17 20:48:26 -0300

    Summary - OK

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { Header } from "./components/Header";
+import { Dashboard } from "./Dashboard";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
   return (
     <>
         <Header />
+        <Dashboard />
         <GlobalStyle />
     </>
   );
