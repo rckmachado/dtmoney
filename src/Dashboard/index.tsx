1bfd3653e51bfa2bb5721990632474aa3409cd6d
Rick Machado <contato.rickmachado@gmail.com>
2022-04-17 21:14:59 -0300

    TransactionTable - OK

--- src/Dashboard/index.tsx
+++ src/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Summary } from "../components/Summary";
+import { TransactionTable } from "../components/TransactionTable";
 import { Container } from "./styles";
 
 export function Dashboard() {
     return (
         <Container>
             <Summary />
+            <TransactionTable />
         </Container>
     );
 }
